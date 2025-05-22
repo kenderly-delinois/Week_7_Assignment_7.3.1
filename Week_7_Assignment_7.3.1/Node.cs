9572cd71039f24438a7db17e7933c87bb269dd9f
Kenderly Delinois <kenderly.delinois1@outlook.com>
2025-05-22 10:01:05 -0500

    Add project files.

---
+++ Week_7_Assignment_7.3.1/Node.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_7_Assignment_7._3._1
+{
+    class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int data, Node left, Node right) { 
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class BST
+    {
+        public Node root; // staring point 
+
+        public BST()
+        {
+            root = null;
+        }
+
+        public void InsertNode(Node temproot, int val)
+        {
+            Node newNode = new Node(val, null ,null);
+            Node temp = null;
+            if(root != null)
+            {
+                while(temproot != null)
+                {
+                    temp = temproot;
+                    if(temproot.data == val)
+                        return;
+                    else if(val < temproot.data) 
+                        temproot = temproot.left;
+                    else if (val > temproot.data)
+                        temproot = temproot.right;
+                }
+                if(val < temp.data)
+                    temp.left = newNode;
+                else if(val > temp.data)
+                    temp.right = newNode;
+            }
+            else
+            {
+                root = newNode;
+            }
+        }
+
+        public void InOrder(Node temproot)
+        {
+            if (temproot != null)
+            {
+                InOrder(temproot.left);
+                Console.WriteLine(temproot.data + " ");
+                InOrder(temproot.right);
+            }
+            
+        }
+
+        public void PreOrder(Node temproot)
+        {
+            if(temproot != null)
+            {
+                Console.WriteLine(temproot.data + " ");
+                PreOrder(temproot.left);
+                PreOrder(temproot.right);
+            }
+        }
+
+        public void PostOrder(Node temproot)
+        {
+            if(temproot != null){
+                PostOrder(temproot.left);
+                PostOrder(temproot.right);
+                Console.WriteLine(temproot.data + " ");
+            }
+        }
+
+        public Node Search(int n)
+        {
+            Node temp = root;
+            while (temp != null)
+            {
+                if(temp.data == n)
+                {
+                    return temp;
+                }
+                else if(n < temp.data)
+                {
+                    temp = temp.left;
+                }
+                else if (n > temp.data)
+                {
+                    temp = temp.right;
+                }
+                    
+            }
+            return temp;
+        }
+    }
+}
---
+++ Week_7_Assignment_7.3.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Linq;
+
+namespace Week_7_Assignment_7._3._1
+{
+    //
+    internal class Program
+    {
+        //You are given the root of a binary search tree(BST) and an integer val.
+        //Find the node in the BST that the node's value equals val 
+        //and return the subtree rooted with that node. 
+        //If such a node does not exist, return null.
+
+
+        static void Main(string[] args)
+        {
+            BST tree = new BST();
+            tree.InsertNode(tree.root, 50);
+            tree.InsertNode(tree.root, 40);
+            tree.InsertNode(tree.root, 70);
+            tree.InsertNode(tree.root, 45);
+            tree.InsertNode(tree.root, 55);
+            tree.InsertNode(tree.root, 60);
+            tree.InsertNode(tree.root, 35);
+
+            Console.WriteLine("Inorder: (Note: Left, Root, Right)");
+            tree.InOrder(tree.root);
+
+            Console.WriteLine("\n\nPreOrder: (Note: Root, Left, Right)");
+            tree.PreOrder(tree.root);
+
+            Console.WriteLine("\n\nPostOrder: (Note: Left, Right, Root)");
+            tree.PostOrder(tree.root);
+
+            Console.WriteLine("\n\nSearching for this specific Node: (Node is 50)");
+            Node found = tree.Search(50);
+
+            if (found != null)
+            {
+                Console.WriteLine("Node found: " + found.data);
+                if(found.left != null)
+                    Console.WriteLine("Left child: " + found.left.data);
+                if (found.right != null)
+                    Console.WriteLine("Right child: " + found.right.data);
+            }
+            else
+            {
+                Console.WriteLine("Node not found");
+            }
+        }
+    }
+}
